2d105bf8e30a118d8f08bbb962109b25e7b9cc84
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-06 12:07:27 +0000

    ...

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Main
+'''
+
+import numpy as np
+import pandas as pd
+import json as js
+import datetime as dt
+
+
+stock_A_df = pd.read_csv('data/stock_A.csv')
+
+print(stock_A_df)
+print('\n')
+
--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-print('hello world')
+'''
 
-
-
-list_ = [1,2,3]
-
-import pandas as pd
-
-d = {'col1': [1, 2], 'col2': [3, 4]}
-df = pd.DataFrame(data=d)
-
-print(df)
+'''

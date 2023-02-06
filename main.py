8e2069da44331747e310933dd4df702cd933e3ca
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-06 17:41:16 +0000

    ...

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ import string
 from cleaning import cleaning_stats_df
 from cleaning import stock_df_lst
 
+
+stock_A_df = stock_df_lst[0]
+
+plt.figure()
+plt.plot(stock_A_df['ts'][0::1000], stock_A_df['price'][0::1000])
+plt.show()

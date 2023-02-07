0dbf5c29075a6de5fe031bbf534b178f680087b8
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-07 12:56:00 +0000

    processing

--- processing.py
+++ processing.py
@@ -20,16 +20,17 @@ from cleaning import stock_df_lst as stock_df_lst_clean
 #Normalize data FOR STOCK A
 
 stock_A_df_clean = stock_df_lst_clean[0]
-print(stock_A_df_clean.describe())
+# print(stock_A_df_clean.describe())
 
-numeric_columns = stock_A_df_clean.select_dtypes(include=[np.number]).columns
-stock_A_df_clean[numeric_columns] = stock_A_df_clean[numeric_columns].apply(sp.stats.zscore)
+# numeric_columns = stock_A_df_clean.select_dtypes(include=[np.number]).columns
+# stock_A_df_clean[numeric_columns] = stock_A_df_clean[numeric_columns].apply(sp.stats.zscore)
 stock_A_df = stock_A_df_clean
 
-print(stock_A_df.head())
-
-
-#.diff().dropna()
+# plt.figure()
+# stock_A_df.price.diff().dropna().plot(title = 'price')
+# # stock_A_df.volume.plot(title = 'volume')
+# plt.grid()
+# plt.show()
 
 
 
--- test.py
+++ test.py
@@ -1,9 +1,3 @@
 '''
 Test
 '''
-
-import pandas as pd
-
-df = pd.DataFrame([['A', 0, 0, 0]], columns = ['1', '2', '3', '4'])
-
-print(df)

e5f12119c294c025801f9311d971d15dfec2bd43
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-07 16:45:52 +0000

    Calculating 30-Minute RV from resamples 1-minute data

--- main.py
+++ main.py
@@ -17,9 +17,16 @@ from processing import stock_df_processed_lst as stock_df_lst
 stock_A_df = stock_df_lst[0]
 
 stock_A_df['1-Minute Return'] = stock_A_df.shift(1)['price'] - stock_A_df['price']
+#Drop duplicates (Non buisness days)
+stock_A_df = stock_A_df.drop_duplicates(subset='1-Minute Return')
+#Drop empty rows (First row)
 stock_A_df = stock_A_df.dropna()
 
 #Calculate 1-day RV (relative vortility)
-stock_A_df['30-Minute Relative Vortility'] = stock_A_df['30-Minute Relative Vortility'].rolling(30).std()
+stock_A_df['30-Minute Relative Vortility'] = stock_A_df['1-Minute Return'].rolling(30).std()
+stock_A_df = stock_A_df.dropna()
 
-print(stock_A_df.head())
+# plt.figure()
+# stock_A_df['30-Minute Relative Vortility'].plot()
+# plt.show()
+

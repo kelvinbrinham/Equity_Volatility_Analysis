4ab4c1bc5e91e8db7526f3bf94cb547ea721a688
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-07 17:48:37 +0000

    RV

--- main.py
+++ main.py
@@ -22,10 +22,19 @@ stock_A_df = stock_A_df.drop_duplicates(subset='1-Minute Return')
 #Drop empty rows (First row)
 stock_A_df = stock_A_df.dropna()
 
+
+def RealisedVolatility(x):
+    return (1 / 10) * np.sqrt(sum([y ** 2 for y in x]))
+
+
 #Calculate 1-day RV (relative vortility)
-stock_A_df['30-Minute Relative Vortility'] = stock_A_df['1-Minute Return'].rolling(30).std()
+stock_A_df['10-Minute Relative Vortility'] = stock_A_df['1-Minute Return'].rolling(10).apply(RealisedVolatility)
 stock_A_df = stock_A_df.dropna()
 
+#Define trading year as 252 days, each with 
+# stock_A_df['10-Minute Relative Vortility (Annualised)'] = stock_A_df['10-Minute Relative Vortility'] * np.sqrt(252/)
+
+print(stock_A_df[:1100])
 # plt.figure()
 # stock_A_df['30-Minute Relative Vortility'].plot()
 # plt.show()
--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 '''
 Test
 '''
+
+
+def sos(x):
+    return sum([y ** 2 for y in x])
+
+
+lst = [1, 3, 5]
+
+print(sos(lst))

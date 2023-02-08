9524da577d3dd5676196bdea08aa729887fe988d
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-08 17:57:33 +0000

    chunking data into daily data

--- processing.py
+++ processing.py
@@ -21,12 +21,39 @@ from cleaning import market_hours
 def RealisedVolatility(x):
     return np.sqrt(sum([y ** 2 for y in x]))
 
-
-def is_5_minutes(x): #CHANGE TO NEW CHUNKED METHOD
-    if x.total_seconds() == 300:
-        return x
-    else:
+def Same_day(x):
+    if x.total_seconds() == 0 or np.isnat(x):
         return np.nan
+    else:
+        return 1
 
 
 stock_df_processed_lst = []
+
+# for i in range(len(stock_df_lst_clean)):
+for i in range(1):
+    stock_letter_df_clean = stock_df_lst_clean[i]
+    stock_letter_df_chunked_lst = []
+
+    # Set timestamp as index
+    stock_letter_df_processing = stock_letter_df_clean.set_index('ts')
+
+    #Create date column so as to chunk dataframes
+    stock_letter_df_processing['Date'] = pd.to_datetime(stock_letter_df_processing.index)
+    stock_letter_df_processing['Date'] = stock_letter_df_processing['Date'].dt.date
+
+    #Chunk dataframe into dataframes for each day
+    stock_letter_df_processing['Date'] = stock_letter_df_processing['Date'] - stock_letter_df_processing['Date'].shift()
+    stock_letter_df_processing['Date'] = stock_letter_df_processing['Date'].apply(Same_day)
+    print(stock_letter_df_processing)
+
+    
+
+        
+    # stock_letter_df_chunk = 
+
+    # print(stock_letter_df_processing.head())
+    # break
+
+
+print('END')

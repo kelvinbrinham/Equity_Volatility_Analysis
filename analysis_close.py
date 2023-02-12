51fbe13e7ab31010ea8c6b474af270ec6ca3a272
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-12 13:09:09 +0000

    ADF test for daily returns

--- analysis_close.py
+++ analysis_close.py
@@ -15,44 +15,25 @@ import scipy as sp
 
 from processing import stock_df_processed_lst
 
-stock_A_close_df = stock_df_processed_lst[0][['Close']]
 
-#Define week as 5 trading days
+stock_A_close_df = stock_df_processed_lst[1][['Close']]
+stock_A_close_df['Date_Temp'] = stock_A_close_df.index 
+stock_A_close_df['Date_Temp'] = stock_A_close_df['Date_Temp'].dt.normalize()
+stock_A_close_df = stock_A_close_df.set_index('Date_Temp')
+stock_A_close_df['Daily pcnt return'] = stock_A_close_df['Close'].pct_change()
+stock_letter_df = stock_A_close_df.dropna()
+stock_letter_df = stock_letter_df.drop(columns = ['Close'])
 
-stock_A_close_df = stock_A_close_df.rolling(5).mean()
-stock_A_close_df = stock_A_close_df.dropna()
-stock_A_close_df['Rolling 5 day return'] = (stock_A_close_df['Close'] - stock_A_close_df['Close'].shift(5)) / stock_A_close_df['Close'].shift(5)
-stock_A_close_df = stock_A_close_df.dropna()
-stock_A_close_df = stock_A_close_df.apply(sp.stats.zscore)
-
-
-from statsmodels.tsa.arima.model import ARIMA
-
-stock_A_df = stock_A_close_df.drop(columns = ['Close'])
-
-stock_A_df = stock_A_df.resample('D').mean()
+print(stock_letter_df.head())
 
 
-# Train test split
-n = int(len(stock_A_df) * 0.75)
-train = stock_A_df['Rolling 5 day return'][:n]
-test = stock_A_df['Rolling 5 day return'][n:]
+#Perform ADF test for stationarity of our daily percentage returns
+from statsmodels.tsa.stattools import adfuller
 
-model = ARIMA(train, order=(8, 0, 1))
-result = model.fit()
-
-
-fig, ax = plt.subplots(figsize=(15, 5))
-
-# Plot the data (here we are subsetting it to get a better look at the forecasts)
-stock_A_df['Rolling 5 day return'].plot(ax=ax)
-
-
-
-# Construct the forecasts
-fcast = result.get_forecast().summary_frame()
-print(fcast)
-fcast['mean'].plot(ax=ax, style='k--')
-ax.fill_between(fcast.index, fcast['mean_ci_lower'], fcast['mean_ci_upper'], color='k', alpha=0.1)
-
-plt.show()
+result = adfuller(stock_letter_df['Daily pcnt return'])
+print(result)
+print('ADF statistic = ', result[0]) 
+print('p-value = %.20f' % result[1]) 
+print('1% = ', result[4]['1%']) 
+print('5% = ', result[4]['5%']) 
+print('10% = ', result[4]['10%']) 
--- test.py
+++ test.py
@@ -2,3 +2,45 @@
 Test
 '''
 
+
+# #Define week as 5 trading days
+
+# stock_A_close_df = stock_A_close_df.rolling(5).mean()
+# stock_A_close_df = stock_A_close_df.dropna()
+# stock_A_close_df['Rolling 5 day return'] = (stock_A_close_df['Close'] - stock_A_close_df['Close'].shift(5)) / stock_A_close_df['Close'].shift(5)
+# stock_A_close_df = stock_A_close_df.dropna()
+# stock_A_close_df = stock_A_close_df.apply(sp.stats.zscore)
+
+
+# from statsmodels.tsa.arima.model import ARIMA
+
+# stock_A_df = stock_A_close_df.drop(columns = ['Close'])
+
+# stock_A_df = stock_A_df.resample('D').ffill()
+# # print(stock_A_df.head())
+
+
+# # Train test split
+# n = int(len(stock_A_df) * 0.75)
+# n = len(stock_A_df) - 1
+# train = stock_A_df['Rolling 5 day return'][:n]
+# test = stock_A_df['Rolling 5 day return'][n:]
+
+# model = ARIMA(train, order=(8, 0, 1))
+# result = model.fit()
+
+
+# fig, ax = plt.subplots(figsize=(15, 5))
+
+# # Plot the data (here we are subsetting it to get a better look at the forecasts)
+# stock_A_df['Rolling 5 day return'].plot(ax=ax)
+
+
+
+# # Construct the forecasts
+# fcast = result.get_forecast(5).summary_frame()
+# print(fcast)
+# fcast['mean'].plot(ax=ax, style='k--')
+# ax.fill_between(fcast.index, fcast['mean_ci_lower'], fcast['mean_ci_upper'], color='k', alpha=0.1)
+
+# plt.show()

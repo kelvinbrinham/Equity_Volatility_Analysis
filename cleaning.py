07f7940f9e4bdfa51864698e566b2dc630450673
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-02-19 11:30:07 +0000

    Plotting

--- cleaning.py
+++ cleaning.py
@@ -28,6 +28,9 @@ for letter in [*string.ascii_uppercase][:4]:
 
     #Import raw data
     stock_letter_df_unclean = pd.read_csv(filename)
+
+    plt.figure()
+    plt.plot(stock_letter_df_unclean.index, stock_letter_df_unclean.price, label = 'Before Cleaning')
     
     #Length before cleaning
     stock_letter_df_unclean_length = len(stock_letter_df_unclean)
@@ -98,8 +101,14 @@ for letter in [*string.ascii_uppercase][:4]:
     if abs(stock_letter_df_unclean.price.values[-1] - stock_letter_df_unclean.price.values[-2]) > outlier_cutoff_dict[letter]:
         stock_letter_df_unclean = stock_letter_df_unclean.drop(index = stock_letter_df_unclean.index[-1], axis = 0)
 
+    plt.plot(stock_letter_df_unclean.index, stock_letter_df_unclean.price, label = 'After Cleaning')
+    plt.title('Stock A')
+    plt.xlabel('Price')
+    plt.legend()
+    plt.savefig('data/price.png', format = 'png', dpi = 800)
+    plt.show()
 
-
+    break
 
     stock_df_lst.append(stock_letter_df_unclean)
     cleaning_stats_df_lst.append(stock_letter_data_cleaning_stats_df)
